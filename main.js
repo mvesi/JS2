77a92d596781bf3417bdd44d51e30370a4fe9328
Michael Genovesi <michaelgenovesi@gmail.com>
2015-04-06 11:32:13 -0600

    first commit & completed

---
+++ main.js
@@ -0,0 +1,11 @@
+var word = prompt("Please enter a word");
+
+alert(word+"\n"+
+	word.length+"\n"+
+	word.charAt(2)+"\n"+
+	word.toLowerCase()+"\n"+
+	word.toUpperCase()+"\n"+
+	"I am putting the word "+word+" in a sentence"+"\n"+
+	word.substring(1,4)
+	);
+
